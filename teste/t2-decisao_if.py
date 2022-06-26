84a58d8050570540291a6b2886b697da2c10c2a4
Daniel <daniel37santana@outlook.com>
2022-06-26 19:30:28 -0300

    if_decisão

---
+++ teste/t2-decisao_if.py
@@ -0,0 +1,7 @@
+nome=input("Digite o nome: ")
+idade=int(input("Digite sua idade: "))
+if idade>=65:
+       print("O paciente " + nome + " POSSUI atendimento prioritário!")
+else:
+        print("O paciente " + nome + " NÂO possui atendimento prioritário")
+
